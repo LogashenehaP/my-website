2f951d809bbc4311afeee89869a8c66df207c4a2
LogashenehaP <113924154+LogashenehaP@users.noreply.github.com>
2023-02-23 11:00:14 +0530

    Update footer.js

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -7,8 +7,8 @@ import { readBlockConfig, decorateIcons } from '../../scripts/lib-franklin.js';
 
 export default async function decorate(block) {
   const cfg = readBlockConfig(block);
+  console.log(cfg);
   block.textContent = '';
-console.log(cfg);
   const footerPath = cfg.footer || '/footer';
    //console.log(footerPath); returns /footer
 //   const resp = 'a';
