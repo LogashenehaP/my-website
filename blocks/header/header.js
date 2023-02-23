dfdbdc4b7b7c3b80b9f89f819ec3bf8ce3345690
LogashenehaP <113924154+LogashenehaP@users.noreply.github.com>
2023-02-23 12:50:59 +0530

    Update header.js

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -93,7 +93,7 @@ export default async function decorate(block) {
   block.textContent = '';
 
   // fetch nav content
-  const navPath = getMetadata('navigation') || '/navigation';
+  const navPath = getMetadata('nav') || '/nav';
   console.log(navPath);
   const resp = await fetch(`${navPath}.plain.html`, window.location.pathname.endsWith('/nav') ? { cache: 'reload' } : {});
 
