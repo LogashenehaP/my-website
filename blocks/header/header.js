965bfc813e44eff849202dd5b951023fcf146a62
LogashenehaP <113924154+LogashenehaP@users.noreply.github.com>
2023-02-27 11:54:57 +0530

    Update header.js

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -108,7 +108,7 @@ export default async function decorate(block) {
     });
 
     const navSections = nav.querySelector('.nav-sections');
-    cosole.log(navSections);
+    console.log(navSections);
     if (navSections) {
       navSections.querySelectorAll(':scope > ul > li').forEach((navSection) => {
         if (navSection.querySelector('ul')) navSection.classList.add('nav-drop');
